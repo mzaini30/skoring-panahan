23b6b795c43f5ea62b92b8ad43e32c926ba544c2
mzaini30 <muhzaini30@gmail.com>
2021-11-11 04:09:11 +0800

    Publish pertama

--- app/src/main/java/com/mzaini30/skoringpanahan/MainActivity.java
+++ app/src/main/java/com/mzaini30/skoringpanahan/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mzaini30.odoj;
+package com.mzaini30.skoringpanahan;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // my banner ad id: ca-app-pub-2878374163061282/6615438108
         // change to your own ad id
         // Change in MainActivity.java and activity_main.xml
-        MobileAds.initialize(this, "ca-app-pub-9804745932553029/6143600976");
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
         mAdView = (AdView)findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
         // replace this with your own link/web app address
         if (savedInstanceState == null) {
-            webview.loadUrl("file:///android_asset/site/index.html");
+            webview.loadUrl("file:///android_asset/public/index.html");
         }
     }
 
